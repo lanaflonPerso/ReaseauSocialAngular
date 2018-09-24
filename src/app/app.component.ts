66a8a09d4d86196fe856466e3f7466272c30d9be
vianneyba <vianneyba@free.fr>
2018-09-24 23:48:00 +0200

    Ajout => formulaires inscription

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'bookshelves';
+  constructor() {
+    var config = {
+      apiKey: "AIzaSyCVTSGVwGjlbN8T7hOJa5XtfxydxebYYYU",
+      authDomain: "utilisateur-2076f.firebaseapp.com",
+      databaseURL: "https://utilisateur-2076f.firebaseio.com",
+      projectId: "utilisateur-2076f",
+      storageBucket: "utilisateur-2076f.appspot.com",
+      messagingSenderId: "520462388903"
+    };
+    firebase.initializeApp(config);
+  }
 }
